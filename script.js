7f0cd1d23c8b7044770d2a53c48bc40e613bd6bb
edmarktuazon <edmarktuazon03@gmail.com>
2023-07-07 19:53:30 +0800

    beshy update

--- script.js
+++ script.js
@@ -4,13 +4,11 @@ const inputFields = document.querySelector("#inputFields");
 const copyButton = document.querySelector("#copyButton");
 
 const fieldFunction = (e) => {
-	var keyCode = event.keyCode || event.which; // Key code for older browsers
+	var keyCode = e.keyCode || e.which; // Key code for older browsers
 	var keyValue = String.fromCharCode(keyCode); // Key value
 
 	if (keyCode === 32 || keyValue === " ") {
-		// The space bar was pressed
-		console.log("Space bar was pressed");
-		inputFields.value += "🤸 ";
+		inputFields.value += "000";
 	}
 };
 
